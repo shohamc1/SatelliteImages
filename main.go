c26baa6576e96524ef62e9c7e18309df5c7e2d75
Shoham Chakraborty <shhmchk@gmail.com>
2021-05-10 14:34:03 +0800

    use dotenv, more configurable

--- main.go
+++ main.go
@@ -10,30 +10,57 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	records := readCSV("russia.csv")
-
-	for idx, record := range records[1:] {
-		os.MkdirAll(fmt.Sprint(idx), 0755)
-		generateImages(record[0], record[1], idx)
-		println(fmt.Sprint(idx), "done")
+	// load env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
 	}
 
+	apiKey := os.Getenv("API_KEY")
+
+	records := readCSV("russia.csv")
+
+	for idx, record := range records[1:2] {
+		os.MkdirAll(fmt.Sprint(idx), 0755)
+		generateImages(record[0], record[1], idx, apiKey)
+		println(fmt.Sprint(idx), "done")
+	}
 }
 
-func generateImages(lat string, long string, idx int) {
+func createSlice(start, end, step float64) []float64 {
+	if step <= 0 || end < start {
+		return []float64{}
+	}
+	s := make([]float64, 0, int(1+(end-start)/step))
+	for start <= end {
+		s = append(s, start)
+		start += step
+	}
+	return s
+}
+
+func generateImages(lat string, long string, idx int, apiKey string) {
 	latF64, _ := strconv.ParseFloat(lat, 64)
 	longF64, _ := strconv.ParseFloat(long, 64)
 
-	lats := [3]float64{latF64 - 0.005, latF64, latF64 + 0.005}
-	longs := [3]float64{longF64 - 0.005, longF64, longF64 + 0.005}
+	var lats = createSlice(latF64-0.01, latF64+0.01, 0.005)
+	var longs = createSlice(longF64-0.01, longF64+0.01, 0.005)
 
-	tuples := [9][2]float64{{lats[0], longs[0]}, {lats[1], longs[0]}, {lats[2], longs[0]}, {lats[0], longs[1]}, {lats[1], longs[1]}, {lats[2], longs[1]}, {lats[0], longs[2]}, {lats[1], longs[2]}, {lats[2], longs[2]}}
+	var tuples [][]float64
+
+	for _, i := range lats {
+		for _, j := range longs {
+			tuples = append(tuples, []float64{i, j})
+		}
+	}
 
 	for _, tuple := range tuples {
-		resp, err := http.Get(fmt.Sprintf("https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=17&size=1280x1280&scale=2&maptype=satellite&key=", tuple[0], tuple[1]))
+		resp, err := http.Get(fmt.Sprintf("https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=17&size=1280x1280&scale=2&maptype=satellite&key=%s", tuple[0], tuple[1], apiKey))
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -43,7 +70,7 @@ func generateImages(lat string, long string, idx int) {
 			log.Fatalln(err)
 		}
 
-		f, err := os.Create(fmt.Sprint(idx, "\\", time.Now().UnixNano(), ".png"))
+		f, err := os.Create(fmt.Sprint(idx, "/", time.Now().UnixNano(), ".png"))
 		if err != nil {
 			log.Fatalln(err)
 		}
